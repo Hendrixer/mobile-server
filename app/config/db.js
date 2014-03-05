82f4df76ae4aced57425a11649345ee38afd9269
Hendrixer <willscottmoss@gmail.com>
2014-03-04 16:15:04 -0800

    (bug) update db uri

--- app/config/db.js
+++ app/config/db.js
@@ -1,12 +1,7 @@
 
 module.exports = function(mongoose){
-  var dbUrl;
-  // update this with more env variables like heroku
-  if(!process.env.URL){
-    dbUrl = 'mongodb://localhost';
-  }
 
-  mongoose.connect(dbUrl + '/ionic');
+  mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/ionic');
   db = mongoose.connection;
   db.on('open', console.log.bind(console, 'connected to db'));
   db.on('error', console.error.bind(console, 'error connecting to db'));
